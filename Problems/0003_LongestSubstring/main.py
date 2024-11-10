ea28eba5bf3341587ee604667972358841dea6f1
calebsenm <calebsenm@gmail.com>
2024-11-10 16:21:47 -0500

    Ok

---
+++ Problems/0003_LongestSubstring/main.py
@@ -0,0 +1,12 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+        return 
+#--------------------------
+substring = "abbbbbb"
+solution = Solution()
+
+num =  solution.lengthOfLongestSubstring(substring) 
+print(num)
+
+
